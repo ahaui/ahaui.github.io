4ef5be56ccd5e7bb7b61605f85f1db4a4838ff80
Kyle(Tình Vũ) <tinhvqbk@gmail.com>
2022-11-25 18:38:20 +0700

    Create lp.js

---
+++ lp.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const colors = {
+  white: '#ffffff',
+  primary: '#00A0E3',
+  warning: '#ffc400'
+}
+
+const getEl = (id) => {
+  const element = document.querySelector('.js-' + id);
+  if (!element) {
+    throw new Error('Element not found: ' + id);
+  }
+  return element;
+};
+
+const tracking = (event, data) => {
+  if (window.gtag) {
+    {
+      window.gtag('event', event, {
+        ...data,
+        version: configs.version,
+      });
+    }
+  }
+};
+
+console.log('AppJS Loading...')
+
+const handler = () => {
+  console.log('AppJS Loaded!!');
+};
+
+document.addEventListener('DOMContentLoaded', handler);
