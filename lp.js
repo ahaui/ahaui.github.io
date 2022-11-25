e0dad92f33772db3f2f16e3978ec8e5e467e59bf
Kyle(Tình Vũ) <tinhvqbk@gmail.com>
2022-11-25 18:56:23 +0700

    Update lp.js

--- lp.js
+++ lp.js
@@ -25,10 +25,36 @@ const tracking = (event, data) => {
   }
 };
 
-console.log('AppJS Loading...')
-
 function handler(){
   console.log('AppJS Loaded!!')
+  // Quick navigation
+  const header = getEl('header');
+  header.style = ' position: sticky; top: 0;'
+  const headerHeight = header.clientHeight;
+  const navigationList = ['how-it-works','benefits','pricing'];
+  const pricingSection =  getEl('section-' + item);
+  navigationList.forEach((item) => {
+    const navigationItem = getEl('navigation-item-' + item);
+    navigationItem.style = `border-bottom: 2px solid transparent; padding-bottom: 4px;`;
+    const section = getEl('section-' + item);
+    navigationItem.addEventListener('click', () => {
+      navigationItem.style.setProperty('border-color', `${colors.warning}`);
+      navigationItem.style.setProperty('color', `${colors.warning}`);
+      navigationList.filter((i) => i !== item).forEach((i) => {
+        const anotherItem = getEl('navigation-item-' + i);
+        anotherItem.style.setProperty('border-color', 'transparent');
+        anotherItem.style.setProperty('color', `${colors.white}`);
+      });
+      window.scrollTo({
+        top: section.offsetTop - headerHeight,
+        behavior: 'smooth'
+      });
+      tracking('select_content', {
+        content_type: item.replace('-', '_'),
+        item_id: 'main_navigation',
+      });
+    });
+  });
 }
 
 handler();
